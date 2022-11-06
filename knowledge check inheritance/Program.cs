a577af20f7c03ee7738ee0c508f02730aaf9c1fa
bishop-casey <112920878+bishop-casey@users.noreply.github.com>
2022-11-06 18:01:18 -0500

    Add files via upload

---
+++ knowledge check inheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using knowledge_check_inheritance;
+
+Console.WriteLine("How many records do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+
+var recordList = new List<charmander>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+    // In this loop, populate the object's properties using Console.ReadLine()
+    var stuff = new charmander();
+
+    Console.WriteLine("What kind of attack: ");
+    stuff.AttackCanPerform = Console.ReadLine();
+
+    Console.WriteLine("What type is it: ");
+    stuff.TypeofAttack = Console.ReadLine();
+
+    recordList.Add(new charmander() { AttackCanPerform = stuff.AttackCanPerform, TypeofAttack = stuff.TypeofAttack });
+}
+// Print out the list of records using Console.WriteLine()
+foreach (var entry in recordList)
+    Console.WriteLine("Attack they can perform " + entry.AttackCanPerform + " Type of attack " + entry.TypeofAttack);
---
+++ knowledge check inheritance/charmander.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace knowledge_check_inheritance
+{
+    internal class charmander : Pokemon
+    {
+        public string? TypeofAttack { get; set; }
+    }
+}
